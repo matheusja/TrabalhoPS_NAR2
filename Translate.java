33d638084a194c53c2ac3379d1b474fffb8543e0
Jordana Luft <jordana.sluft@gmail.com>
2019-05-31 12:04:37 -0300

    Translate

--- Translate.java
+++ Translate.java
@@ -1,88 +1,92 @@
+
 /*
 Classe recebe uma string com o local onde o arquivo fonte esta
 para começar a execução
 Retorna um vetor de inteiros convertidos para dec
-
  */
-package tanslate;
+package translate;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 /**
  *
- * @author Usuario
+ * @author Pablo
  */
 public class Translate {
+
     //String onde vai ser armazenada o arquivo
-    private String str="";
+    private String str = "";
     //tamanho de linhas do arquivo
-    private int tm=0;
+    private int tm = 1;
     //vetor de sub Strings Geradas para tradução
     private String subStrins[];
-    //Arquivo carregado
-    private BufferedReader br;
     //vator de inteiros
     private int decString[];
-    
- 
+
     //construtor onde recebe o arquivo a
-    public Translate( String local){
-        String line;
-        //abre o arquivo
-        try{
-              br = new BufferedReader(new FileReader(local));
-             while(br.ready()) {
-                line = br.readLine();
-                //concatena as linhas
-                str= str+line+'\n';
+    public Translate(String local) {
+        str = local;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '\n') {
                 tm++;
 
-
-             }
-             br.close();
-        }catch(IOException ioe){
-             ioe.printStackTrace();
-        }
-    }
-    /** converte de binario para (inteiro)
-      * Aceita uma string de tamanho arbitrário(4, 16, 42 caracteres)
-      * Os primeiros bits tem o menor valor;
-      */
-    public int binTodec(String bin ) {
-        int val = 0;
-        for(int i = 0; i < bin.length();i++) {
-            if(bin[i]) == '1') {
-                val | 1;
             }
-            val = val << 1;
+
         }
-        return val;
+
     }
+
+    public static int binToDecUnsigned(String bin) {
+        double dec = 0;
+        int tam = 0;
+        for (int i = bin.length() - 1; i > -1; i--) {
+
+            if (bin.charAt(i) == '1') {
+                dec = dec + (Math.pow(2, tam));
+            }
+            tam++;
+        }
+
+        return (int) dec;
+    }
+
+    //converte de binario para dec
+    public static int binToDecSigned(String bin) {
+        int unsigned = binToDecUnsigned(bin.substring(1));
+        if (bin.charAt(0) == '1') {
+            return -(int) unsigned;
+        }
+        return (int) unsigned;
+
+    }
+
     //gera as subs string
-    void    setSubStrings( ) {
-        subStrins=new String[tm];
-        int x=0; 
-        for(int i=0;i<tm;i++){
-            subStrins[i]="";
+    void setSubStrings() {
+        subStrins = new String[tm];
+        int x = 0;
+        for (int i = 0; i < tm; i++) {
+            subStrins[i] = "";
         }
-        for(int j =0;j<str.length();j++){
-            subStrins[x]+= str.charAt(j);
-            if(str.charAt(j)=='\n'){
-                x++;
-            }
-         }
-       for(int i=0;i<tm;i++){
 
-       }
-   } 
+        for (int j = 0; j < str.length(); j++) {
+            if (str.charAt(j) == '\n') {
+                x++;
+            } else {
+                subStrins[x] += str.charAt(j);
+            }
+        }
+    }
+
     //retorna os valores em dec
     public int[] convertReturn() {
-        decString= new int [tm];
-        for(int i=0;i<tm;i++) {
-            decString[i]= binTodec(subStrins[i]);
+        setSubStrings();
+        decString = new int[tm];
+        for (int i = 0; i < tm; i++) {
+            decString[i] = binToDecSigned(subStrins[i]);
         }
         return decString;
     }
-}
 
+}
