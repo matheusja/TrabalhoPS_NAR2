5c38de11b8fb68f13b615f22d94c22c9ffd0b700
PabloLSF <45604802+PabloLSF@users.noreply.github.com>
2019-07-05 12:11:21 -0300

    Update Struct.java

--- projeto_NAR2/src/view/Struct.java
+++ projeto_NAR2/src/view/Struct.java
@@ -1,4 +1,4 @@
-/*
+//*
  Class Struct recebe do tradutor uma string com tamanho 32 bits
 e as divide em opcode, indice, flags e parametros
  */
@@ -19,6 +19,12 @@ public class Struct {
     private String pa="";
     private String bin="";
     private boolean P, R, I, N;
+    private String parBin;
+    private String inBin;
+    private String opBin;
+    private String flagBin;
+    private boolean pBin,rBin,iBin,nBin;
+    
     
         public Struct() {
           P = R = I = N = false;
@@ -79,6 +85,63 @@ public class Struct {
                }
                parametro=Translate.binToDecSigned(pa);
            }
+          //set os paraetros par atraves de um inteiro
+          public void setParBin(int n) {
+              String aux;
+              aux=Translate.decTobin(n);
+              parBin="";
+              for(int i=31;i>15;i--){
+              parBin+=aux.charAt(i);
+              }
+              
+          }
+          //set os opcode par atraves de um inteiro
+          public void setOpBin(int n) {
+              opBin="";
+             String aux;
+              aux=Translate.decTobin(n);
+              for(int i=31;i>23;i--){
+                 opBin+=aux.charAt(i);
+              }
+          }
+          //set os indice par atraves de um inteiro
+          public void setInBin(int n) {
+              inBin="";
+              String aux;
+              aux=Translate.decTobin(n);
+              for(int i=31;i>27;i--){
+                 inBin+=aux.charAt(i);
+              }
+          }
+          //set a flag par atraves de um inteiro
+          public void setFlagBin(int n) {
+              flagBin="";
+              String aux;
+              aux=Translate.decTobin(n);
+                for(int i=31;i>27;i--){
+                   flagBin +=aux.charAt(i);
+                }
+              switch(n) {
+        case 0:
+        P = R = I = N = false;
+        break;
+        case 1:
+            P =true;
+        break;
+        case 2:
+            R=true;
+        break;
+       case 3:
+           I=true;
+        break;
+         case 4:
+           N=true;
+        break;
+    
+  
+    
+}
+          }
               public int getOpDec (){
                  
                   return opcode;
@@ -113,6 +176,33 @@ public class Struct {
               }
               public boolean getisJump() {
                   return false; // Implementar
+              }
+              
+               public String getFlagBinEntrada (){
+                  
+                  return flagBin;
+              }
+              public String getOpBinEntrada  (){
+                  
+                  return opBin;
+              }
+              public String getInBinEntrada (){
+                  
+                  return inBin;
+              }
+              public String getPaBinEntrada (){
+                  
+                  return parBin;
+              }
+              //retorna uma instrução
+               public String getInstru (){
+                  String inst="";
+                  inst=parBin;
+                  inst=inst+flagBin;
+                  inst=inst+inBin;
+                  inst=inst+opBin;
+                  
+                  return inst;
               }
 }
 
