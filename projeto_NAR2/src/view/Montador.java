49fab631293a4d373be3131b9faf9572ff236077
André Zucchetti Dalla Costa <andrezdc02@gmail.com>
2019-07-05 13:22:11 -0300

    Update Montador.java

--- projeto_NAR2/src/view/Montador.java
+++ projeto_NAR2/src/view/Montador.java
@@ -5,6 +5,7 @@
  */
 package montador;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -16,6 +17,7 @@ public class Montador
     
     String code;
     int index;
+    HashMap<String, Integer> instructions;
     
     CodigoMontado codigoMontado;
     
@@ -23,6 +25,7 @@ public class Montador
         this.code = code;
         this.index = 0;
         codigoMontado = new CodigoMontado();
+        instructions = loadInstructions();//new HashMap<String, Integer>();
     }
     
     private CodigoMontado montarInterno() throws Exception {
@@ -41,10 +44,11 @@ public class Montador
         // <nada>:
         String lastToken = null;
         int pc = 0;
-        while(index < code.length());
+        while(index < code.length())
         {
             String currentToken = nextToken();
             if (currentToken.equals(":")) {
+                //pc--;
                 if (lastToken == null) {
                     throw new Exception("Simbolo sem nome");
                 }
@@ -53,6 +57,11 @@ public class Montador
                 } else {
                     codigoMontado.tabelaDeSimbolos.put(lastToken, pc);
                 }
+            }
+            else
+            {
+                lastToken = currentToken;
+                pc++;
             }
             
         }
@@ -99,11 +108,81 @@ public class Montador
     private void passo2() throws Exception {
         // Levantar erro para simbolo n/ definido que nao eh externo
         
+        
+        String lastToken = null;
+        int pc = 0;
+        
+        int instructionInt;
+                
+        while(index < code.length())
+        {
+            String currentToken = nextToken();
+            
+            
+            if(instructions.get(currentToken) != null)
+            {
+                instructionInt = instructions.get(currentToken);
+                codigoMontado.codigo.add(instructionInt);
+            }
+            if (currentToken.equals(":")) {
+                //pc--;
+                if (lastToken == null) {
+                    throw new Exception("Simbolo sem nome");
+                }
+                if (codigoMontado.tabelaDeSimbolos.containsKey(lastToken)) {
+                    throw new Exception("Simbolo duplamente definido");
+                } else {
+                    codigoMontado.tabelaDeSimbolos.put(lastToken, pc);
+                }
+            }
+            else
+            {
+                //lastToken = currentToken;
+                pc++;
+            }
+            
+        }
+        
+        
     }
     
     public static CodigoMontado montar(String code) throws Exception {
         Montador mn = new Montador(code);
         return mn.montarInterno();
+    }
+    
+    
+    private HashMap loadInstructions()
+    {
+        HashMap<String, Integer> inst = new HashMap<String, Integer>();
+        
+        int i = 0;
+        
+        inst.put("ZAR",i++);
+        inst.put("MUA",i++);
+        inst.put("AUM",i++);
+        inst.put("PIR",i++);
+        inst.put("SABF",i++);
+        inst.put("ODUF",i++);
+        inst.put("MNOF",i++);
+        inst.put("DELF",i++);
+        inst.put("PZAF",i++);
+        inst.put("SAB",i++);
+        inst.put("ODU",i++);
+        inst.put("MNO",i++);
+        inst.put("DEL",i++);
+        inst.put("PZA",i++);
+        inst.put("KON",i++);
+        inst.put("DIS",i++);
+        inst.put("NEG",i++);
+        inst.put("POL",i++);
+        inst.put("POD",i++);
+        inst.put("NES",i++);
+        inst.put("BES",i++);
+        inst.put("NUS",i++);
+                                
+                
+        return inst;
     }
     
 }
