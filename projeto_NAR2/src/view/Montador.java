683c41faf73e3d23866f5279b44a848ab528e359
André Zucchetti Dalla Costa <andrezdc02@gmail.com>
2019-07-05 11:30:56 -0300

    Update Montador.java

--- projeto_NAR2/src/view/Montador.java
+++ projeto_NAR2/src/view/Montador.java
@@ -5,6 +5,8 @@
  */
 package montador;
 
+import java.util.Scanner;
+
 /**
  *
  * @author andrezdc
@@ -13,9 +15,13 @@ public class Montador
 {
     
     String code;
+    int index;
+    
     CodigoMontado codigoMontado;
+    
     public Montador(String code) {
         this.code = code;
+        this.index = 0;
         codigoMontado = new CodigoMontado();
     }
     
@@ -31,9 +37,65 @@ public class Montador
         // SPACE <tamanho>
         // '<label>':
         // Levantar erro para simbolo definido 2x
-        
+        // Levantar erro para label vazio   
+        // <nada>:
+        String lastToken = null;
+        int pc = 0;
+        while(index < code.length());
+        {
+            String currentToken = nextToken();
+            if (currentToken.equals(":")) {
+                if (lastToken == null) {
+                    throw new Exception("Simbolo sem nome");
+                }
+                if (codigoMontado.tabelaDeSimbolos.containsKey(lastToken)) {
+                    throw new Exception("Simbolo duplamente definido");
+                } else {
+                    codigoMontado.tabelaDeSimbolos.put(lastToken, pc);
+                }
+            }
+            
+        }
         
     }
+    
+    private String nextToken() {
+        ignorarEspacos();
+        String str = "";
+        char c = code.charAt(index);
+        if (c == ':') {
+            return ":";
+        }
+        while (index < code.length() && !ignoravel(c)) {
+            if (c == ':') {
+                return str;
+            }
+            str += c;
+            index++;
+            c = code.charAt(index);
+        }
+        
+        return str; 
+    }
+    
+    private void ignorarEspacos() {
+        char c = code.charAt(index);
+        while (index < code.length() && ignoravel(c)) {
+            index++;
+            if (c == '#') {
+                // Comment
+                while(index < code.length() && c != '\n') {
+                    index++;
+                } 
+            }
+        }
+    }
+    
+    private boolean ignoravel(char c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == '#';
+    }
+    
+    
     private void passo2() throws Exception {
         // Levantar erro para simbolo n/ definido que nao eh externo
         
